c3973b13629f0d294ca9f22b80b103683c2cdf29
SunnyGZY <670453367@qq.com>
2018-02-27 11:12:59 +0800

    修改项目图标

--- app/src/androidTest/java/com/sunny/www/compass/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sunny/www/compass/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.sunny.www.compass", appContext.getPackageName());
+        assertEquals("com.sunny.www.ic_launcher", appContext.getPackageName());
     }
 }
