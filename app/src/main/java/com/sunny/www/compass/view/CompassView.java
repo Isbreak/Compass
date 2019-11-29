6918eb7768a56d29dec4e5c8805644b34d801507
gl <670453367@qq.com>
2019-11-29 13:43:59 +0800

    CompassView 绘制优化

--- app/src/main/java/com/sunny/www/compass/view/CompassView.java
+++ app/src/main/java/com/sunny/www/compass/view/CompassView.java
@@ -20,15 +20,17 @@ import com.sunny.www.compass.utils.DisplayUtil;
 public class CompassView extends View {
 
     private Canvas mCanvas;
-    private Paint mPaint;
     private Rect mTextRect;
 
     private float directionAngle = 0;
     private float oldDirectionAngle = 0;
 
+    /**
+     * dp 转 px，尺寸适配
+     */
     private int textDirSize = DisplayUtil.sp2px(getContext(), 11);
-    private int textMidAngelSize = DisplayUtil.sp2px(getContext(), 51);
-    private int textRudAngelSize = DisplayUtil.sp2px(getContext(), 7);
+    private int textMidAngleSize = DisplayUtil.sp2px(getContext(), 51);
+    private int textRudAngleSize = DisplayUtil.sp2px(getContext(), 7);
     private int inOvalSize = DisplayUtil.dp2px(getContext(), 113);
     private int outOvalSize = inOvalSize + DisplayUtil.dp2px(getContext(), 34);
     private int inOvalStrokeWidth = DisplayUtil.dp2px(getContext(), 2);
@@ -36,15 +38,14 @@ public class CompassView extends View {
     private int scaleLength = DisplayUtil.dp2px(getContext(), 11);
     private int normalScaleWidth = DisplayUtil.dp2px(getContext(), 1.1f);
     private int specialScaleWidth = DisplayUtil.dp2px(getContext(), 1.3f);
-    private int trigonSize = DisplayUtil.dp2px(getContext(), 19);
+    private int triangleSize = DisplayUtil.dp2px(getContext(), 19);
+    private int blackTriangleSize = DisplayUtil.dp2px(getContext(), 22);
     private int spaceSize = DisplayUtil.dp2px(getContext(), 6);
 
 
     public CompassView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
         mTextRect = new Rect();
     }
 
@@ -73,10 +74,10 @@ public class CompassView extends View {
         drawOutSideArc();
 
         // 画灰色三角形
-        drawGrayTrigon();
+        drawGrayTriangle();
 
         // 画中间角度值
-        drawMidAngel();
+        drawMidAngle();
 
         // 画内部圆弧
         drawInSideArc();
@@ -84,7 +85,7 @@ public class CompassView extends View {
         mCanvas.rotate(-directionAngle, 0f, 0f);
 
         // 画红色三角形
-        drawRedTrigon();
+        drawRedTriangle();
 
         // 画表盘内的刻度线，刻度值及方向
         drawOthers();
@@ -92,7 +93,8 @@ public class CompassView extends View {
 
     // 绘制最外部两个圆弧
     private void drawOutSideArc() {
-        Paint outsideArcPaint = new Paint(mPaint);
+        Paint outsideArcPaint = new Paint();
+        outsideArcPaint.setAntiAlias(true);
         outsideArcPaint.setStyle(Paint.Style.STROKE);
         outsideArcPaint.setColor(Color.argb(255, 50, 50, 50));
         outsideArcPaint.setStrokeWidth(outOvalStrokeWidth);
@@ -102,106 +104,117 @@ public class CompassView extends View {
     }
 
     // 画灰色三角形
-    private void drawGrayTrigon() {
-        Paint grayTrigonPaint = new Paint(mPaint);
-        grayTrigonPaint.setColor(Color.argb(255, 50, 50, 50));
+    private void drawGrayTriangle() {
+        Paint grayTrianglePaint = new Paint();
+        grayTrianglePaint.setAntiAlias(true);
+        grayTrianglePaint.setColor(Color.argb(255, 50, 50, 50));
         Path grayTriangle = new Path();
-        grayTriangle.moveTo(-trigonSize / 2, -outOvalSize + outOvalStrokeWidth);
-        grayTriangle.lineTo(trigonSize / 2, -outOvalSize + outOvalStrokeWidth);
-        grayTriangle.lineTo(0, -outOvalSize - (int) (Math.sqrt(trigonSize * trigonSize - trigonSize / 2 * trigonSize / 2) + 0.5) + outOvalStrokeWidth);
+        grayTriangle.moveTo(-triangleSize / 2, -outOvalSize + outOvalStrokeWidth);
+        grayTriangle.lineTo(triangleSize / 2, -outOvalSize + outOvalStrokeWidth);
+        grayTriangle.lineTo(0, -outOvalSize - (int) (Math.sqrt(triangleSize * triangleSize - triangleSize / 2 * triangleSize / 2) + 0.5) + outOvalStrokeWidth);
         grayTriangle.close();
-        mCanvas.drawPath(grayTriangle, grayTrigonPaint);
+        mCanvas.drawPath(grayTriangle, grayTrianglePaint);
     }
 
     // 画中间角度值
-    private void drawMidAngel() {
-        String angel = String.valueOf((int) directionAngle);
-        Paint midAngelPaint = new Paint(mPaint);
-        midAngelPaint.setColor(Color.argb(255, 252, 252, 252));
-        midAngelPaint.setTextSize(textMidAngelSize);
-        midAngelPaint.getTextBounds(angel, 0, angel.length(), mTextRect);
-
-        Paint.FontMetricsInt fm = midAngelPaint.getFontMetricsInt();
+    private void drawMidAngle() {
+        String angle = String.valueOf((int) directionAngle);
+        Paint midAnglePaint = new Paint();
+        midAnglePaint.setAntiAlias(true);
+        midAnglePaint.setColor(Color.argb(255, 252, 252, 252));
+        midAnglePaint.setTextSize(textMidAngleSize);
+        midAnglePaint.getTextBounds(angle, 0, angle.length(), mTextRect);
+        Paint.FontMetricsInt fm = midAnglePaint.getFontMetricsInt();
         float height = -fm.descent + (fm.descent - fm.ascent) / 2;
-
-        // 使用 measureText() 代替原先的 mTextRect.width() 方法,文字居中效果更好
-        float width = midAngelPaint.measureText(angel);
-        mCanvas.drawText(angel + "°", -width / 2, height, midAngelPaint);
+        float width = midAnglePaint.measureText(angle);
+        mCanvas.drawText(angle + "°", -width / 2, height, midAnglePaint);
     }
 
 
     // 画内部圆弧
     private void drawInSideArc() {
+        mCanvas.rotate(-90, 0f, 0f);
+
         // 红色画笔
-        Paint redArcPaint = new Paint(mPaint);
+        Paint redArcPaint = new Paint();
+        redArcPaint.setAntiAlias(true);
         redArcPaint.setColor(Color.argb(255, 253, 57, 0));
         redArcPaint.setStyle(Paint.Style.STROKE);
         redArcPaint.setStrokeWidth(inOvalStrokeWidth);
 
         // 灰色画笔
-        Paint grayArcPaint = new Paint(mPaint);
+        Paint grayArcPaint = new Paint();
+        grayArcPaint.setAntiAlias(true);
         grayArcPaint.setColor(Color.argb(255, 155, 155, 155));
         grayArcPaint.setStyle(Paint.Style.STROKE);
         grayArcPaint.setStrokeWidth(inOvalStrokeWidth);
 
         RectF insideOval = new RectF(-inOvalSize, -inOvalSize, inOvalSize, inOvalSize);
 
-        if (directionAngle < 180) {
-            if (directionAngle > 7) {
-                mCanvas.drawArc(insideOval, -90 - directionAngle + 6, directionAngle - 1 - 6, false, redArcPaint);
-            }
+        // 手机方向传感器采集到的角度为逆时针，转为顺时针，便于绘图
+        float angle = 360 - directionAngle;
+        // 圆环中间顶部缺口大小
+        float space = 1;
 
-            if (directionAngle <= 4) {
-                mCanvas.drawArc(insideOval, -90 + 5 - directionAngle, 349, false, grayArcPaint);
-            } else {
-                mCanvas.drawArc(insideOval, -90 + 1, 360 - directionAngle - 7, false, grayArcPaint);
-            }
-
+        if (angle < 180) {
+            mCanvas.drawArc(insideOval, 0 + space, angle, false, redArcPaint);
+            mCanvas.drawArc(insideOval, angle, 360 - space - angle, false, grayArcPaint);
         } else {
-            if (directionAngle < 353) {
-                mCanvas.drawArc(insideOval, -90 + 1, 360 - directionAngle - 7, false, redArcPaint);
-            }
-
-            if (directionAngle >= 354) {
-                mCanvas.drawArc(insideOval, -90 + 5 + 360 - directionAngle, 349, false, grayArcPaint);
-            } else {
-                mCanvas.drawArc(insideOval, -90 + 360 - directionAngle + 6, directionAngle - 1 - 6, false, grayArcPaint);
-            }
-
+            mCanvas.drawArc(insideOval, angle, 360 - space - angle, false, redArcPaint);
+            mCanvas.drawArc(insideOval, 0 + space, angle, false, grayArcPaint);
         }
 
+        mCanvas.rotate(90, 0f, 0f);
     }
 
     // 画红色三角形
-    private void drawRedTrigon() {
-        //fd3900
-        // 背景 5A5D63
-        Paint redTrigonPaint = new Paint(mPaint);
-        redTrigonPaint.setColor(Color.argb(255, 253, 57, 0));
+    private void drawRedTriangle() {
+        // 在红色三角形背后画一个稍微大一点的黑色三角形，实现 padding 效果
+        Paint blackTrianglePaint = new Paint();
+        blackTrianglePaint.setAntiAlias(true);
+        blackTrianglePaint.setColor(Color.argb(255, 0, 0, 0));
+        Path blackTriangle = new Path();
+        blackTriangle.moveTo(-blackTriangleSize / 2, -inOvalSize + inOvalStrokeWidth);
+        blackTriangle.lineTo(blackTriangleSize / 2, -inOvalSize + inOvalStrokeWidth);
+        blackTriangle.lineTo(0, -inOvalSize - (int) (Math.sqrt(blackTriangleSize * blackTriangleSize - blackTriangleSize / 2 * blackTriangleSize / 2) + 0.5) + inOvalStrokeWidth);
+        blackTriangle.close();
+        mCanvas.drawPath(blackTriangle, blackTrianglePaint);
+
+        Paint redTrianglePaint = new Paint();
+        redTrianglePaint.setAntiAlias(true);
+        redTrianglePaint.setColor(Color.argb(255, 253, 57, 0));
         Path redTriangle = new Path();
-        redTriangle.moveTo(-trigonSize / 2, -inOvalSize + inOvalStrokeWidth);
-        redTriangle.lineTo(trigonSize / 2, -inOvalSize + inOvalStrokeWidth);
-        redTriangle.lineTo(0, -inOvalSize - (int) (Math.sqrt(trigonSize * trigonSize - trigonSize / 2 * trigonSize / 2) + 0.5) + inOvalStrokeWidth);
+        redTriangle.moveTo(-triangleSize / 2, -inOvalSize + inOvalStrokeWidth);
+        redTriangle.lineTo(triangleSize / 2, -inOvalSize + inOvalStrokeWidth);
+        redTriangle.lineTo(0, -inOvalSize - (int) (Math.sqrt(triangleSize * triangleSize - triangleSize / 2 * triangleSize / 2) + 0.5) + inOvalStrokeWidth);
         redTriangle.close();
-        mCanvas.drawPath(redTriangle, redTrigonPaint);
+        mCanvas.drawPath(redTriangle, redTrianglePaint);
     }
 
     // 画表盘内的刻度线，刻度值及方向
     private void drawOthers() {
         // 普通刻度
-        Paint normalScalePaint = new Paint(mPaint);
+        Paint normalScalePaint = new Paint();
+        normalScalePaint.setAntiAlias(true);
         normalScalePaint.setStyle(Paint.Style.FILL);
         normalScalePaint.setStrokeWidth(normalScaleWidth);
         normalScalePaint.setColor(Color.argb(255, 107, 107, 107));
         // 特殊刻度
-        Paint specialScalePaint = new Paint(mPaint);
+        Paint specialScalePaint = new Paint();
+        specialScalePaint.setAntiAlias(true);
         specialScalePaint.setStyle(Paint.Style.FILL);
         specialScalePaint.setStrokeWidth(specialScaleWidth);
         specialScalePaint.setColor(Color.argb(255, 155, 155, 155));
 
         // 表盘圈内的东西南北
-        Paint dirTextPaint = new Paint(mPaint);
+        Paint dirTextPaint = new Paint();
+        dirTextPaint.setAntiAlias(true);
         dirTextPaint.setTextSize(textDirSize);
+        // 表盘圈内30整倍数刻度值
+        Paint roundAngleTextPaint = new Paint();
+        roundAngleTextPaint.setAntiAlias(true);
+        roundAngleTextPaint.setColor(Color.argb(255, 107, 107, 107));
+        roundAngleTextPaint.setTextSize(textRudAngleSize);
 
         for (int i = 0; i < 360; i++) {
             if (i % 90 == 0) {
@@ -231,15 +244,11 @@ public class CompassView extends View {
                 }
             } else if (i % 30 == 0) {
                 mCanvas.drawLine(0, -inOvalSize + spaceSize, 0, -inOvalSize + scaleLength + spaceSize, specialScalePaint);
-                Paint roundAngelTextPaint = new Paint(mPaint);
-                String angelValue = String.valueOf(i);
-                roundAngelTextPaint.setColor(Color.argb(255, 107, 107, 107));
-                roundAngelTextPaint.setTextSize(textRudAngelSize);
-                roundAngelTextPaint.getTextBounds(angelValue, 0, angelValue.length(), mTextRect);
+                String angleValue = String.valueOf(i);
+                roundAngleTextPaint.getTextBounds(angleValue, 0, angleValue.length(), mTextRect);
                 int textWidth = mTextRect.width();
                 int textHeight = mTextRect.height();
-                mCanvas.drawText(angelValue, -textWidth / 2, textHeight - inOvalSize + scaleLength + (int) (spaceSize * 1.6 + 0.5), roundAngelTextPaint);
-
+                mCanvas.drawText(angleValue, -textWidth / 2, textHeight - inOvalSize + scaleLength + (int) (spaceSize * 1.6 + 0.5), roundAngleTextPaint);
             } else if (i % 2 == 0) {
                 mCanvas.drawLine(0, -inOvalSize + spaceSize, 0, -inOvalSize + scaleLength + spaceSize, normalScalePaint);
             }
