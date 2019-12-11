83c67d79508a719ff477628e8db956e5418a862c
670453367 <670453367@qq.com>
2019-12-11 22:12:56 +0800

    删了点东西

--- app/src/main/java/com/sunny/www/compass/MainActivity.java
+++ app/src/main/java/com/sunny/www/compass/MainActivity.java
@@ -202,5 +202,3 @@ public class MainActivity extends AppCompatActivity {
         mSensorManager.unregisterListener(sensorEventListener);
     }
 }
-
-//非常抱歉，好久没有更新了，之前有段时间比较忙，就把APP的签名密钥弄丢了，导致无法升级版本。从2.0.0版本之前升级到此版本的用户，因为签名不一致，会导致安装失败，请先把旧APP卸载，十分抱歉。本次更新添加了手机方向传感器精度检测，出现精度不准提示时，请按照提示去校准哦~
