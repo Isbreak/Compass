72477a0a0cf722f895c2e5de76b12cc4d72c6af4
GaozyDev <670453367@qq.com>
2020-03-02 10:30:44 +0800

    显示经纬度

--- app/src/main/java/com/sunny/www/compass/MainActivity.java
+++ app/src/main/java/com/sunny/www/compass/MainActivity.java
@@ -35,6 +35,8 @@ import com.trycatch.mysnackbar.TSnackbar;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * 指南针主界面
@@ -81,6 +83,10 @@ public class MainActivity extends AppCompatActivity {
 
     private float lastDirAngel = 0;
 
+    private LocationManager locationManager;
+
+    private Location lastLocation;
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -95,186 +101,7 @@ public class MainActivity extends AppCompatActivity {
         initData();
         initView();
         initListener();
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION
-                    }, PERMISSION_REQUEST_CODE);
-        } else {
-            initLocation();
-        }
-    }
-
-    private void initData() {
-        sharedPreferences = getSharedPreferences(SP_CONFIG, Context.MODE_PRIVATE);
-        isFirstOpen = sharedPreferences.getBoolean(IS_FIRST_OPEN, true);
-        isVibrate = sharedPreferences.getBoolean(IS_VIBRATE, true);
-    }
-
-    private void initView() {
-        mViewGroup = (ViewGroup) findViewById(android.R.id.content).getRootView();
-        mDirection = findViewById(R.id.tv_dir);
-        mCompassView = findViewById(R.id.compass);
-        if (isFirstOpen) {
-            showSnackBar();
-        }
-    }
-
-    private void initListener() {
-        mCompassView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if ((System.currentTimeMillis() - lastTime) > 2000) {
-                    lastTime = System.currentTimeMillis();
-                } else {
-                    isVibrate = !isVibrate;
-                    sharedPreferences.edit().putBoolean(IS_VIBRATE, isVibrate).apply();
-                    Toast.makeText(MainActivity.this, isVibrate ? "打开震动" : "关闭震动", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-    private LocationManager locationManager;
-
-    private void initLocation() {
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        assert locationManager != null;
-        if (!(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))) {
-            Toast.makeText(this, "请打开位置服务", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        long minSecond = 2 * 1000;
-        long minDistance = 2;
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (location == null) {
-            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        }
-        updateLocation(location);
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minSecond, minDistance, listener);
-
-        GPSStatusImpl gpsStatusImpl = new GPSStatusImpl();
-        locationManager.addGpsStatusListener(gpsStatusImpl);
-    }
-
-    public final LocationListener listener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            updateLocation(location);
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            switch (status) {
-                // GPS状态为可见时
-                case LocationProvider.AVAILABLE:
-                    Log.i("xml", "当前GPS状态为可见状态");
-                    break;
-                // GPS状态为服务区外时
-                case LocationProvider.OUT_OF_SERVICE:
-                    Log.i("xml", "当前GPS状态为服务区外状态");
-                    break;
-                // GPS状态为暂停服务时
-                case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                    Log.i("xml", "当前GPS状态为暂停服务状态");
-                    break;
-            }
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-            @SuppressLint("MissingPermission")
-            Location location = locationManager.getLastKnownLocation(provider);
-            updateLocation(location);
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-        }
-    };
-
-    private class GPSStatusImpl implements GpsStatus.Listener {
-        @Override
-        public void onGpsStatusChanged(int event) {
-            switch (event) {
-                case GpsStatus.GPS_EVENT_FIRST_FIX:  //第一次定位
-                    break;
-
-                case GpsStatus.GPS_EVENT_SATELLITE_STATUS:  //卫星状态改变
-                {
-                    //获取当前状态
-                    @SuppressLint("MissingPermission")
-                    GpsStatus gpsStatus = locationManager.getGpsStatus(null);
-                    //获取卫星颗数的默认最大值
-                    int maxSatellites = gpsStatus.getMaxSatellites();
-                    //创建一个迭代器保存所有卫星
-                    Iterator<GpsSatellite> iters = gpsStatus.getSatellites().iterator();
-                    int count = 0;
-                    while (iters.hasNext() && count <= maxSatellites) {
-                        GpsSatellite s = iters.next();
-                        // 只有信噪比不为0时才算合格的卫星
-                        if (s.getSnr() != 0) {
-                            count++;
-                        }
-                    }
-
-
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    String curTime = dateFormat.format(new Date());
-                    System.out.println(curTime + "-----卫星数量nun----" + count);
-                    break;
-                }
-
-                case GpsStatus.GPS_EVENT_STARTED:   //定位启动
-                    break;
-
-                case GpsStatus.GPS_EVENT_STOPPED:   //定位结束
-                    break;
-            }
-        }
-    }
-
-    Location lastLocation;
-
-    private void updateLocation(Location location) {
-        String result;
-        double lat;
-        double lon;
-
-        if (location != null) {
-            lat = location.getLatitude();
-            lon = location.getLongitude();
-            if (location.hasSpeed()) {
-//                speed = location.getSpeed() * 3.6;
-            } else {
-                if (lastLocation != null) {
-//                    speed = getMySpeed(lastLocation, location);
-                }
-            }
-
-//            result = "纬度：" + lat + "\n经度：" + lon + "\n速度：" + String.format("%.2f", speed) + " km/h\n卫星数量：" + stars;
-//            Log.i("xml", "显示结果 = " + result);
-//            lastLocation = location;
-
-            TextView tv = findViewById(R.id.tv_lon_lat);
-            tv.setText(lat + " " + lon);
-        }
-    }
-
-    private void showSnackBar() {
-        TSnackbar snackBar = TSnackbar.make(mViewGroup, "点击两次次表盘，可关闭(打开)震动哦", TSnackbar.LENGTH_INDEFINITE, TSnackbar.APPEAR_FROM_TOP_TO_DOWN);
-        snackBar.setAction("确认", new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sharedPreferences.edit().putBoolean(IS_FIRST_OPEN, false).apply();
-            }
-        });
-        snackBar.setPromptThemBackground(Prompt.SUCCESS);
-        snackBar.show();
+        initLocation();
     }
 
     @SuppressWarnings("deprecation")
@@ -317,11 +144,174 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private void initData() {
+        sharedPreferences = getSharedPreferences(SP_CONFIG, Context.MODE_PRIVATE);
+        isFirstOpen = sharedPreferences.getBoolean(IS_FIRST_OPEN, true);
+        isVibrate = sharedPreferences.getBoolean(IS_VIBRATE, true);
+    }
+
+    private void initView() {
+        mViewGroup = (ViewGroup) findViewById(android.R.id.content).getRootView();
+        mDirection = findViewById(R.id.tv_dir);
+        mCompassView = findViewById(R.id.compass);
+        if (isFirstOpen) {
+            showSnackBar();
+        }
+    }
+
+    private void initListener() {
+        mCompassView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((System.currentTimeMillis() - lastTime) > 2000) {
+                    lastTime = System.currentTimeMillis();
+                } else {
+                    isVibrate = !isVibrate;
+                    sharedPreferences.edit().putBoolean(IS_VIBRATE, isVibrate).apply();
+                    Toast.makeText(MainActivity.this, isVibrate ? "打开震动" : "关闭震动", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void initLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                    }, PERMISSION_REQUEST_CODE);
+        } else {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            List<String> providers = locationManager.getProviders(true);
+            String locationProvider;
+            Location location;
+            if (providers.contains(LocationManager.GPS_PROVIDER)) {
+                locationProvider = LocationManager.GPS_PROVIDER;
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            } else if (providers.contains(LocationManager.NETWORK_PROVIDER)) {
+                locationProvider = LocationManager.NETWORK_PROVIDER;
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            } else {
+                Toast.makeText(this, "请打开位置服务", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            updateLocation(location);
+
+            locationManager.requestLocationUpdates(locationProvider, 2000, 2, locationListener);
+            locationManager.addGpsStatusListener(gpsStatusListener);
+        }
+    }
+
+    public final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            updateLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    };
+
+    private GpsStatus.Listener gpsStatusListener = new GpsStatus.Listener() {
+
+        @Override
+        public void onGpsStatusChanged(int event) {
+            switch (event) {
+                case GpsStatus.GPS_EVENT_FIRST_FIX:  //第一次定位
+                    break;
+                case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
+                    //卫星状态改变
+                    //获取当前状态
+                    @SuppressLint("MissingPermission")
+                    GpsStatus gpsStatus = locationManager.getGpsStatus(null);
+                    //获取卫星颗数的默认最大值
+                    int maxSatellites = gpsStatus.getMaxSatellites();
+                    //创建一个迭代器保存所有卫星
+                    Iterator<GpsSatellite> iterator = gpsStatus.getSatellites().iterator();
+                    int count = 0;
+                    while (iterator.hasNext() && count <= maxSatellites) {
+                        GpsSatellite s = iterator.next();
+                        // 只有信噪比不为0时才算合格的卫星
+                        if (s.getSnr() != 0) {
+                            count++;
+                        }
+                    }
+
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
+                    String curTime = dateFormat.format(new Date());
+                    Log.e("gaozy", curTime + "-----卫星数量nun----" + count);
+                    break;
+                case GpsStatus.GPS_EVENT_STARTED:   //定位启动
+                    break;
+                case GpsStatus.GPS_EVENT_STOPPED:   //定位结束
+                    break;
+            }
+        }
+    };
+
+    private void updateLocation(Location location) {
+        double lon;
+        double lat;
+        double speed;
+
+        if (location != null) {
+            lon = location.getLongitude();
+            lat = location.getLatitude();
+            if (location.hasSpeed()) {
+                speed = location.getSpeed() * 3.6;
+
+                TextView tvLon = findViewById(R.id.tv_lon);
+                tvLon.setText(String.format(getString(R.string.format_float_4), lon));
+                TextView tvLat = findViewById(R.id.tv_lat);
+                tvLat.setText(String.format(getString(R.string.format_float_4), lat));
+                TextView tvSpeed = findViewById(R.id.tv_speed);
+                tvSpeed.setText(String.format(getString(R.string.format_float_2), speed));
+            } else {
+                if (lastLocation != null) {
+//                    speed = getMySpeed(lastLocation, location);
+                }
+            }
+            lastLocation = location;
+        }
+    }
+
+    private void showSnackBar() {
+        TSnackbar snackBar = TSnackbar.make(mViewGroup, "点击两次次表盘，可关闭(打开)震动哦", TSnackbar.LENGTH_INDEFINITE, TSnackbar.APPEAR_FROM_TOP_TO_DOWN);
+        snackBar.setAction("确认", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedPreferences.edit().putBoolean(IS_FIRST_OPEN, false).apply();
+            }
+        });
+        snackBar.setPromptThemBackground(Prompt.SUCCESS);
+        snackBar.show();
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
         //注销传感器
         mSensorManager.unregisterListener(sensorEventListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null) {
+            locationManager.removeUpdates(locationListener);
+        }
     }
 
     @Override
